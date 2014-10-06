c79dbad8bdc20f0b399acfd9a2d3ae4c335e00f8
David Davidson <daviddavidson.1988@gmail.com>
2014-10-05 19:59:52 -0700

    Tidy up the first implementation and the README

--- 9/lib/bruteForce.js
+++ 9/lib/bruteForce.js
@@ -1,5 +1,14 @@
 'use strict';
 
+/**
+ * Finds Pythagorean triples that sum to a given number
+ *
+ * Performance: O(N^3) (just a brute-force implementation)
+ *
+ * @param {Number} max: the target sum
+ * @returns {Array} results: matching triples, stored as objects
+ */
+
 var isPythagoreanTriple = function(a, b, c) {
 	return Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2);
 };
