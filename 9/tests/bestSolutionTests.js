2081633ca79ee4638b4ad1bae51c34d8e3c0ff13
David Davidson <daviddavidson.1988@gmail.com>
2014-10-06 12:19:54 -0700

    Add a solution that needs only one for-loop

---
+++ 9/tests/bestSolutionTests.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var chai = require('chai'),
+	expect = chai.expect,
+	best = require('../lib/best'),
+	results = best(1000);
+
+describe('The single-loop solution', function() {
+
+	it('Finds a match given the input of 1,000', function() {
+		expect(results).to.be.ok; // truthy
+	});
+
+	it('Finds a valid Pythagorean triple', function() {
+		var a,
+			b,
+			c;
+
+		expect(results).to.have.property('a');
+		expect(results).to.have.property('b');
+		expect(results).to.have.property('b');
+
+		a = results.a;
+		b = results.b;
+		c = results.c;
+		expect(Math.pow(a, 2) + Math.pow(b, 2)).to.eql(Math.pow(c, 2));
+		expect(a + b + c).to.eql(1000);
+	});
+
+});
